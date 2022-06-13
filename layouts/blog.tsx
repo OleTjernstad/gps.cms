5aececf8c0ad39bb461f3ec7fc0998b8c64a390c
Ole Tjernstad <ott556@gmail.com>
2022-06-13 22:31:59 +0200

    Started on footer and main

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -1,12 +1,16 @@
+import { Footer } from "./blog/footer";
 import { Header } from "./blog/header";
 import { Navbar } from "./blog/navbar";
 import styled from "styled-components";
 
+const Main = styled.main``;
 export const Blog = () => {
   return (
     <>
       <Header />
       <Navbar />
+      <Main>Main</Main>
+      <Footer />
     </>
   );
 };
---
+++ layouts/blog/footer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Logo from "../../public/logo.png";
+import styled from "styled-components";
+
+const FooterElem = styled.footer`
+  background: linear-gradient(
+    115deg,
+    rgba(243, 230, 251, 1) 11%,
+    rgba(182, 148, 233, 1) 100%
+  );
+  width: 100%;
+  height: 120px;
+  margin-top: auto;
+`;
+
+export const Footer = () => {
+  return <FooterElem>Footer</FooterElem>;
+};
